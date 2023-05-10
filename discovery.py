da293a84ff255117b5a74747df6b89dc3d279ffe
Guilherme Henrique de Sousa Jesus <guilherme26h@gmail.com>
2023-05-09 21:57:21 -0300

    Commit de estrutura inicial
    Efetuado a criação do .env e a classe de descoberta para inicialização do projeto

---
+++ discovery.py
@@ -0,0 +1,14 @@
+import socket as s
+from dotenv import load_dotenv
+config = load_dotenv(".env")
+
+#Variaveis Globais
+
+ host = 
+
+def enviarEmail():
+    
+
+def pegarIp()
+    ip = s.gethostbyname(host)
+    enviarEmail(host)
